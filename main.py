fd2365abda56f8b96fac1d781668c8597338f208
Andrei <andreymitrofanov1337@gmail.com>
2025-12-30 16:45:50 +0300

    any comment

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from aiogram.filters import Command
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, WebAppInfo
 
 API_TOKEN = "8592559811:AAH29ce0hb8CQCuT7E_EIY64Nwoi5CILQX8"  # 🔹 вставь сюда токен бота
-BASE_WEBAPP_URL = "https://b15a68ce.my-photo-webapp.pages.dev/"  # 🔹 твой Cloudflare Pages URL
+BASE_WEBAPP_URL = "https://my-photo-webapp.pages.dev/code.html"  # 🔹 твой Cloudflare Pages URL
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
